3d58b89d1784b10cccf0526f3a731eb709d1eea6
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-06-08 17:08:45 +0530

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -7,11 +7,19 @@ const CartSummary = () => (
   <CartContext.Consumer>
     {value => {
       const {cartList} = value
+      let totalAmount = 0
+      /* eslint-disable no-console */
+      /* eslint-disable no-restricted-syntax */
+      for (const product of cartList) {
+        totalAmount += product.quantity * product.price
+      }
+      console.log(totalAmount)
       return (
         <div className="cart-summary-container">
           <div>
             <h1 className="order-total">
-              Order Total: <span className="total-amount">Rs 314950/- </span>
+              Order Total:{' '}
+              <span className="total-amount">Rs {totalAmount}/- </span>
             </h1>
             <p className="cart-count">{cartList.length} items in cart</p>
           </div>
